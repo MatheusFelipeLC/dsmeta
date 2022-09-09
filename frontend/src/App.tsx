fb32074e34803f0c29987f327ef4ab15164ffdd8
Matheus-Nephicide <matheus1993felipe@yahoo.com.br>
2022-09-09 04:25:46 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
+import NotificationButton from './components/NotificationButton'
 function App() {
   return(
+    <>
     <h1>Olá mundo!</h1>
+    <NotificationButton />
+    </>
   )
 }
 
