f1780394da85efc2edbcd09fe6feccb57a7fba81
Matheus <matheus1993felipe@yahoo.com.br>
2022-09-11 05:11:18 -0300

    Sale listing

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -38,7 +38,7 @@ public class SmsService {
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
 		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
-			    + " com um total de R$ " + sale.getAmount();
+			    + " com um total de R$ " + sale.getAmount() + "nosso salário daqui ums meses francisca kellory";
 
 		Twilio.init(twilioSid, twilioKey);
 
