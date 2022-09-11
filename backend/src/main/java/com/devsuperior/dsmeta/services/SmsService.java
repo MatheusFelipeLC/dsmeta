e74ed2e893b4f01feb9be51a7d8333e3835836a1
Matheus <matheus1993felipe@yahoo.com.br>
2022-09-11 05:31:40 -0300

    Date update

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -38,7 +38,8 @@ public class SmsService {
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
 		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
-			    + " com um total de R$ " + sale.getAmount() + "nosso salário daqui ums meses francisca kellory";
+			    + " com um total de R$ " + String.format("%.0f", sale.getAmount());
+
 
 		Twilio.init(twilioSid, twilioKey);
 
